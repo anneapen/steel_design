212734e7f9faee7ab945aa4761c4e4a7cdded0c9
anneapen <annmary181@gmail.com>
2026-08-07 18:01:28 +0530

    updated class_of_bolt()

--- steel_design_calcs.py
+++ steel_design_calcs.py
@@ -10,7 +10,7 @@ def edge_distance (do:float,t:float,fy:float)->tuple[float,float]:
 
 def class_of_bolt(grade:float)->tuple[float,float]:
     """
-    Returns fub and fyb
+    Returns ultimate strength of the bolt,fub and yield strength,fyb for the given grade of bolt
     """
     fub=int(grade)*100
     dec=grade-int(grade)
