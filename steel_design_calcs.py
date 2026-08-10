3973f26fb25f584f1642f74ed595cbdcff7094ce
anneapen <annmary181@gmail.com>
2026-08-10 19:35:20 +0530

    Modified steel_design_calcs

--- steel_design_calcs.py
+++ steel_design_calcs.py
@@ -1,9 +1,45 @@
 import math
 
-def edge_distance (do:float,t:float,fy:float)->tuple[float,float]:
+def class_of_bolt(grade:float)->tuple[float,float]:
+    """
+    Returns ultimate strength of the bolt,fub and yield strength,fyb for the given grade of bolt
+    """
+    fub=int(grade)*100
+    dec=grade-int(grade)
+    fyb=dec*fub
+    return fub,round(fyb,1)
+
+def net_area_of_bolt(d:float)->float:
+    """
+    Returns the net area of the bolt,Anb(mm2)
+    where d- diameter of the bolt,mm 
+    """
+    Asb=math.pi/4*(d**2)
+    Anb=0.78*Asb
+
+    return round(Anb,2)
+
+def hole_diameter(d:float)->float:
+    """
+    IS 800:2007 (Clause 10.2.1), the diameter of a standard bolt hole (d₀) is larger than the nominal diameter of the bolt (d) 
+    to allow easy insertion and account for minor misalignments. For standard clearance holes, the extra clearance added to the bolt 
+    diameter depends on the bolt size: 1.0 mm extra for 12 to 14 mm bolts, 2.0 mm extra for 16 to 24 mm bolts, and 3.0 mm extra for bolts 
+    larger than 24 mm.
+    """
+    if d>12 and d<14:
+        do=d+1
+    elif d>16 and d<24:
+        do=d+2
+    elif d>24:
+        do=d+3
+
+    return do
+
+def edge_distance (d:float,t:float,fy:float)->tuple[float,float]:
     """
     Returns the maximum and minimum edge/end distance
     """
+    do=hole_diameter(d)
     emin=1.5*do
     eps=(250/fy)**(1/2)
     emax=12*t*eps
@@ -27,38 +63,3 @@ def pitch(d:float,t:float,type:str)->tuple[float,float]:
         p_max=min(16*t,200)
     return p_min,p_max
 
-
-def class_of_bolt(grade:float)->tuple[float,float]:
-    """
-    Returns ultimate strength of the bolt,fub and yield strength,fyb for the given grade of bolt
-    """
-    fub=int(grade)*100
-    dec=grade-int(grade)
-    fyb=dec*fub
-    return fub,round(fyb,1)
-
-def net_area_of_bolt(d:float):
-    """
-    Returns the net area of the bolt,Anb(mm2)
-    where d- diameter of the bolt,mm 
-    """
-    Asb=math.pi/4*(d**2)
-    Anb=0.78*Asb
-
-    return round(Anb,2)
-
-def hole_diameter(d:float):
-    """
-    IS 800:2007 (Clause 10.2.1), the diameter of a standard bolt hole (d₀) is larger than the nominal diameter of the bolt (d) 
-    to allow easy insertion and account for minor misalignments. For standard clearance holes, the extra clearance added to the bolt 
-    diameter depends on the bolt size: 1.0 mm extra for 12 to 14 mm bolts, 2.0 mm extra for 16 to 24 mm bolts, and 3.0 mm extra for bolts 
-    larger than 24 mm.
-    """
-    if d>12 and d<14:
-        do=d+1
-    elif d>16 and d<24:
-        do=d+2
-    elif d>24:
-        do=d+3
-
-    return do
--- test_steel_design_calcs.py
+++ test_steel_design_calcs.py
@@ -1,9 +1,5 @@
 import steel_design_calcs as steel_design_calcs
 
-def test_edge_distance():
-    emin,emax=steel_design_calcs.edge_distance(12,10,250)
-    assert emin,emax==(18.0, 120.0)
-
 def test_class_of_bolt():
     fub,fyb=steel_design_calcs.class_of_bolt(4.6)
     assert fub,fyb==(400,240.0)
@@ -15,6 +11,10 @@ def test_net_area_of_bolt():
 def test_hole_diameter():
     do=steel_design_calcs.hole_diameter(20)
     assert do==22
+
+def test_edge_distance():
+    emin,emax=steel_design_calcs.edge_distance(20,10,250)
+    assert emin,emax==(33.0, 120.0)
 
 def test_pitch():
     p_min,p_max=steel_design_calcs.pitch(20,10,'compression')
