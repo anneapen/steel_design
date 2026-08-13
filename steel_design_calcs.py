af1f75bd0303ed192eb2d4e9d977a7c9e3bcbe56
anneapen <annmary181@gmail.com>
2026-08-13 20:08:48 +0530

    included angle_of_fusion and test fn

--- steel_design_calcs.py
+++ steel_design_calcs.py
@@ -129,12 +129,30 @@ def design_strength_of_butt_weld(lw:float,tmin:float,fy:float,fu:float,weld_type
 
    return round(T_dw,2),round(V_dw,2)
 
-def design_strength_of_fillet_weld(s:float,lw:float,weld_type:str,fu:float)->tuple[float,float]:
+def angle_of_fusion(theta:float)->float:
+    """
+    """
+    if 60<theta<90:
+        K=0.7
+    elif 91<theta<100:
+        K=0.65
+    elif 101<theta<106:
+        K=0.6
+    elif 107<theta<113:
+        K=0.55
+    elif 114<theta<120:
+        K=0.5
+    else:
+        K=0.7
+    return K
+
+def design_strength_of_fillet_weld(s:float,lw:float,weld_type:str,fu:float,theta:float)->tuple[float,float]:
     """
     returns the design strength of weld
     """
     
-    tt=0.7*s
+    K=angle_of_fusion(theta)
+    tt=K*s
     if (weld_type=='shop weld'):
           psf=1.25
     elif (weld_type=='field weld'):
--- test_steel_design_calcs.py
+++ test_steel_design_calcs.py
@@ -37,8 +37,12 @@ def test_design_strength_of_butt_weld():
     T_dw=steel_design_calcs.design_strength_of_butt_weld(150,12,240,250,'shop weld','single')[0]
     assert T_dw==225.0
 
+def test_angle_of_fusion():
+    K=steel_design_calcs.angle_of_fusion(60)
+    assert K==0.7
+
 def test_design_strength_of_fillet_weld():
-    V_dw=steel_design_calcs.design_strength_of_fillet_weld(6,216,'shop weld',410)[1]
+    V_dw=steel_design_calcs.design_strength_of_fillet_weld(6,216,'shop weld',410,60)[1]
     assert V_dw==171.8
 
 def test_rolled_steel_beam():
