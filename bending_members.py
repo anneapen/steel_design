ee0881f2c63bbc41c7f481ab2152406afc19b096
anneapen <annmary181@gmail.com>
2026-09-05 14:05:31 +0530

    Update bending_members.py

--- bending_members.py
+++ bending_members.py
@@ -1,6 +1,6 @@
 import math
 
-def bending_strengh_laterally_supported()->float:
+def bending_strengh_laterally_supported(section:str,Ze:float,Zp:float,fy:float,gamma_mo:float)->float:
     """
     """
     if section=='plastic' or section=='compact':
@@ -8,7 +8,7 @@ def bending_strengh_laterally_supported()->float:
     elif section=='semi-compact':
         beta=Ze/Zp
     else:
-        raise ValueError print("Secction should be plastic or compact or semi compact")
+        raise ValueError ("Secction should be plastic or compact or semi compact")
 
     Md=beta*Zp*fy/gamma_mo
     return Md
