046928ca80a14611cee65dc839e9c120288eebf8
anneapen <annmary181@gmail.com>
2026-09-11 17:53:36 +0530

    Added design_shear_strength() to bending_members,py

--- bending_members.py
+++ bending_members.py
@@ -9,7 +9,14 @@ def bending_strengh_laterally_supported(section:str,Ze:float,Zp:float,fy:float,g
     elif section=='semi-compact':
         beta=Ze/Zp
     else:
-        raise ValueError ("Secction should be plastic or compact or semi compact")
+        raise ValueError ("Section should be plastic or compact or semi compact")
 
     Md=beta*Zp*fy/gamma_mo
     return Md
+
+def design_shear_strength(Av:float,fy:float,gamma_mo:float)->float:
+    """
+    Returns the design shear strength
+    """
+    Vd=(Av*fy)/(math.sqrt(3)*gamma_mo)
+    return Vd
