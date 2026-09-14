35cac8c5c1b0a064f4c615fb6dde3e62e4a61014
anneapen <annmary181@gmail.com>
2026-09-14 08:40:07 +0530

    Updated bending_members.py

--- bending_members.py
+++ bending_members.py
@@ -1,6 +1,6 @@
 import math
 
-def bending_strengh_laterally_supported(section:str,Ze:float,Zp:float,fy:float,gamma_mo:float)->float:
+def bending_strengh_laterally_supported(section:str,Ze:float,Zp:float,fy:float,gamma_m0:float)->float:
     """
     Returns the design bending strength for a laterally supported beam
     """
@@ -11,8 +11,8 @@ def bending_strengh_laterally_supported(section:str,Ze:float,Zp:float,fy:float,g
     else:
         raise ValueError ("Section should be plastic or compact or semi compact")
 
-    Md=beta*Zp*fy/gamma_mo
-    Md_limit=(1.2*Ze*fy)//gamma_mo
+    Md=beta*Zp*fy/gamma_m0
+    Md_limit=(1.2*Ze*fy)//gamma_m0
     Md=min(Md,Md_limit)*10**-6
     
     return round(Md,2)
@@ -42,12 +42,12 @@ def bending_strengh_laterally_unsupported(section:str,Ze:float,Zp:float,fy:float
 
 
 
-def design_shear_strength(h:float,tw:float,fy:float,gamma_mo:float)->float:
+def design_shear_strength(h:float,tw:float,fy:float,gamma_m0:float)->float:
     """
     Returns the design shear strength
     """
     Av=h*tw
-    Vd=(Av*fy)/(math.sqrt(3)*gamma_mo)*10**-3
+    Vd=(Av*fy)/(math.sqrt(3)*gamma_m0)*10**-3
     return round(Vd,2)
 
 def flange_plastic_moment(h: float,bf: float,tf: float,fy: float, gamma_m0: float) -> float:
